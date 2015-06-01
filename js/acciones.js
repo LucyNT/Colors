1de5a57a0c0160c05e90035d7bd37a8c957b8b94
AL30F13 <luci.nt@gmail.com>
2015-06-01 13:46:12 -0500

    tres
    cambios

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,15 @@
 // JavaScript Document
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
+	audio=windows.plugins.LowLatencyAudio;
+	audio.preloadFX('B1', 'audio/C.mp3',function(){},
+	function(msg){alert("error "+msg);});
+	audio.preloadFX('B2', 'audio/D.mp3',function(){},
+	function(msg){alert("error "+msg);});
+	audio.preloadFX('B3', 'audio/E.mp3',function(){},
+	function(msg){alert("error "+msg);});
+	audio.preloadFX('B4', 'audio/F.mp3',function(){},
+	function(msg){alert("error "+msg);});
 	$('#btnjugar').on('tap',function(){
 		var pantalla = $.mobile.getScreenHeight();
 		var encabezado = $('.ui-header').outerHeight(); 
@@ -14,9 +23,9 @@ document.addEventListener("deviceready",function(){
     
 	function quien (q)
 	{
+		audio.play(q);
 		return q.substring(1);
 	}
-	
    $('.cuadro').on('vmousedown',function(){
 	   $('#pantalla').append(quien($(this).attr('id')));
 	   $(this).addClass('pulsado');
